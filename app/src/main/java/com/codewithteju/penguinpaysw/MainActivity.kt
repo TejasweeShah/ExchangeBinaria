e9bebdd7776b71e956a8fdcde70dd3481579a9ee
Tejaswee Shah <tejaswee.shah@gmail.com>
2022-07-06 11:26:34 -0400

    Initial commit

---
+++ app/src/main/java/com/codewithteju/penguinpaysw/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.codewithteju.penguinpaysw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
